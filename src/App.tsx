1236505e00c416c49af7087311e8b6107cf60c22
Igor Minayeu <igor.minayeu@gmail.com>
2023-11-02 20:17:36 +0300

    add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+export type BanknotesType = "All" | 'Rubles' | 'Dollars'
+export type MoneyType = {
+    id: string
+    banknotes: BanknotesType
+    value: number
+    numberId: string
+}
+
+const defaultMoney: MoneyType[] = [
+    {id: crypto.randomUUID(), banknotes: 'Rubles', value: 100, numberId: 'j23j42k3kre'},
+    {id: crypto.randomUUID(), banknotes: 'Dollars', value: 100, numberId: 'f23j42k3kre'},
+    {id: crypto.randomUUID(), banknotes: 'Rubles', value: 100, numberId: 'j73j42k3kre'},
+    {id: crypto.randomUUID(), banknotes: 'Dollars', value: 100, numberId: 'k23j42k3kre'},
+    {id: crypto.randomUUID(), banknotes: 'Dollars', value: 100, numberId: 'r73j42k3kre'},
+    {id: crypto.randomUUID(), banknotes: 'Rubles', value: 100, numberId: 'o23j42k3kre'},
+    {id: crypto.randomUUID(), banknotes: 'Rubles', value: 100, numberId: 'p23j42k3kre'},
+    {id: crypto.randomUUID(), banknotes: 'Dollars', value: 100, numberId: 'w23j42k3kre'},
+    {id: crypto.randomUUID(), banknotes: 'Dollars', value: 100, numberId: 'v23j42k3kre'},
+    {id: crypto.randomUUID(), banknotes: 'Rubles', value: 100, numberId: 'n73j42k3kre'}
+]
+
+const filterMoney = (money: MoneyType[], filter: BanknotesType): MoneyType[] => {
+    return filter === "All" ? money : money.filter(m => m.banknotes === filter)
+}
+
 function App() {
+
+    const [money, setMoney] = useState<MoneyType[]>(defaultMoney)
+    const [filter, setFilter] = useState<BanknotesType>('All')
+    const filteredMoney = filterMoney(money, filter)
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+
         </div>
     );
 }
